b7ce6eeb993a8603b82fa09b68f53c077ab64f8f
Chichirau Razvan <chichiraurazvan@yahoo.com>
2024-01-20 17:48:39 +0200

    feat(seq2seq): Refactored code + generated html plots

--- src/seq2seq/seq2seqService.py
+++ src/seq2seq/seq2seqService.py
@@ -2,81 +2,176 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Dropout, LayerNormalization, TimeDistributed
+from tensorflow.keras.layers import (
+    Input,
+    Dense,
+    Dropout,
+    LayerNormalization,
+    TimeDistributed,
+)
 from tensorflow.keras.layers import MultiHeadAttention  # Import direct din TensorFlow
 import matplotlib.pyplot as plt
 from tensorflow.keras.optimizers import Adam
+import plotly.graph_objects as go
 
 from src.extractor import extract_dataset
 
-data = extract_dataset()
 
-columns_to_predict = ['pres', 'temp1', 'umid', 'temp2', 'V450', 'B500', 'G550', 'Y570', 'O600', 'R650', 'temps1',
-                      'temps2', 'lumina']
-df = pd.DataFrame(data[1:], columns=['Timestamp'] + columns_to_predict)
+class Seq2SeqService:
+    """
+    Service for constructing, training and generating plots for the Seq2Seq model
+    """
 
-df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-df[columns_to_predict] = df[columns_to_predict].apply(pd.to_numeric)
-df.set_index('Timestamp', inplace=True)
+    def __init__(self):
+        self.data = extract_dataset()
+        self.path = "../../static/predictions_plots/seq2seq"
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(df)
+        self.n_steps_in = 168
+        self.n_steps_out = 168
 
-def create_sequences(data, n_steps_in, n_steps_out):
-    X, y = [], []
-    for i in range(len(data)):
-        end_ix = i + n_steps_in
-        out_end_ix = end_ix + n_steps_out
-        if out_end_ix > len(data):
-            break
-        seq_x, seq_y = data[i:end_ix], data[end_ix:out_end_ix]
-        X.append(seq_x)
-        y.append(seq_y)
-    return np.array(X), np.array(y)
+        self.columns_to_predict = [
+            "pres",
+            "temp1",
+            "umid",
+            "temp2",
+            "V450",
+            "B500",
+            "G550",
+            "Y570",
+            "O600",
+            "R650",
+            "temps1",
+            "temps2",
+            "lumina",
+        ]
+        self.df = pd.DataFrame(
+            data[1:], columns=["Timestamp"] + self.columns_to_predict
+        )
 
-n_steps_in = 168
-n_steps_out = 168
+        self.df["Timestamp"] = pd.to_datetime(self.df["Timestamp"])
+        self.df[self.columns_to_predict] = self.df[self.columns_to_predict].apply(
+            pd.to_numeric
+        )
+        self.df.set_index("Timestamp", inplace=True)
 
-train_data = scaled_data[:-n_steps_out]
-test_data = scaled_data[-n_steps_out:]
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.scaled_data = self.scaler.fit_transform(self.df)
 
-X_train, y_train = create_sequences(train_data, n_steps_in, n_steps_out)
+        self.train_data = self.scaled_data[: -self.n_steps_out]
+        self.test_data = self.scaled_data[-self.n_steps_out :]
 
-# Define the input layer
-input_seq = Input(shape=(n_steps_in, len(columns_to_predict)))
+        self.X_train, self.y_train = self._create_sequences(
+            self.train_data, self.n_steps_in, self.n_steps_out
+        )
 
-# Transformer Encoder
-encoder_output = MultiHeadAttention(num_heads=2, key_dim=len(columns_to_predict)//2)(query=input_seq, value=input_seq)
-encoder_output = Dropout(0.1)(encoder_output)
-encoder_output = LayerNormalization(epsilon=1e-6)(input_seq + encoder_output)
+        # Define the input layer
+        self.input_seq = Input(shape=(self.n_steps_in, len(self.columns_to_predict)))
 
-# Transformer Decoder
-decoder_output = MultiHeadAttention(num_heads=2, key_dim=len(columns_to_predict)//2)(query=encoder_output, value=encoder_output)
-decoder_output = Dropout(0.1)(decoder_output)
-decoder_output = LayerNormalization(epsilon=1e-6)(encoder_output + decoder_output)
+        # Transformer Encoder
+        encoder_output = MultiHeadAttention(
+            num_heads=2, key_dim=len(self.columns_to_predict) // 2
+        )(query=self.input_seq, value=self.input_seq)
+        encoder_output = Dropout(0.1)(encoder_output)
+        encoder_output = LayerNormalization(epsilon=1e-6)(
+            self.input_seq + encoder_output
+        )
 
-# Output layer
-output = TimeDistributed(Dense(len(columns_to_predict)))(decoder_output)
+        # Transformer Decoder
+        decoder_output = MultiHeadAttention(
+            num_heads=2, key_dim=len(self.columns_to_predict) // 2
+        )(query=encoder_output, value=encoder_output)
+        decoder_output = Dropout(0.1)(decoder_output)
+        decoder_output = LayerNormalization(epsilon=1e-6)(
+            encoder_output + decoder_output
+        )
 
-model = Model(inputs=input_seq, outputs=output)
-model.compile(optimizer=Adam(learning_rate=0.01), loss='mse')
+        # Output layer
+        self.output = TimeDistributed(Dense(len(self.columns_to_predict)))(
+            decoder_output
+        )
 
-model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2)
+    def _create_sequences(self, data, n_steps_in, n_steps_out):
+        X, y = [], []
+        for i in range(len(data)):
+            end_ix = i + n_steps_in
+            out_end_ix = end_ix + n_steps_out
+            if out_end_ix > len(data):
+                break
+            seq_x, seq_y = data[i:end_ix], data[end_ix:out_end_ix]
+            X.append(seq_x)
+            y.append(seq_y)
+        return np.array(X), np.array(y)
 
-new_predictions = model.predict(train_data[-n_steps_in:].reshape(1, n_steps_in, len(columns_to_predict)))
+    def train(self, epochs: int = 100, batch_size: int = 32):
+        """
+        Trains the model with number of epochs and batch size specified
 
-predicted_values_original = scaler.inverse_transform(new_predictions.reshape(-1, len(columns_to_predict)))
-test_data_original = scaler.inverse_transform(test_data)
+        Args:
+            epochs (int): Number of epochs to train the model
+            batch_size (int): Batch size to train the model
 
-for i, col in enumerate(columns_to_predict):
-    plt.figure(figsize=(12, 4))
+        Return: None
+        """
 
-    prediction_times = pd.date_range(start=df.index[-n_steps_in], periods=n_steps_out+1, freq='H')[1:]
-    plt.plot(prediction_times, test_data_original[:, i], label=f'Actual {col}', linestyle='-', marker='')
-    plt.plot(prediction_times, predicted_values_original[:, i], label=f'Predicted {col}', linestyle='-', marker='')
+        self.model = Model(inputs=self.input_seq, outputs=self.output)
+        self.model.compile(optimizer=Adam(learning_rate=0.01), loss="mse")
 
-    plt.title(f'Evoluția valorii {col} în ultima săptămână')
-    plt.xlabel('Timestamp')
-    plt.ylabel('Value')
-    plt.legend()
-    plt.show()
+        self.model.fit(
+            self.X_train,
+            self.y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_split=0.2,
+        )
+
+        new_predictions = self.model.predict(
+            self.train_data[-self.n_steps_in :].reshape(
+                1, self.n_steps_in, len(self.columns_to_predict)
+            )
+        )
+
+        self.predicted_values_original = self.scaler.inverse_transform(
+            new_predictions.reshape(-1, len(self.columns_to_predict))
+        )
+        self.test_data_original = self.scaler.inverse_transform(self.test_data)
+
+    def generate_plots(self):
+        """
+        Generates all the plots for the predicted values
+
+        Return: None
+        """
+        for i, col in enumerate(self.columns_to_predict):
+            fig = go.Figure()
+            fig.add_trace(
+                go.Scatter(
+                    x=self.df.index[-self.n_steps_in :],
+                    y=self.df[col].values[-self.n_steps_in :],
+                    mode="lines",
+                    name=f"Actual {col}",
+                )
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=self.df.index[-self.n_steps_in :],
+                    y=self.predicted_values_original[:, i],
+                    mode="lines",
+                    name=f"Predicted {col}",
+                )
+            )
+            fig.update_layout(
+                title=f"Predictions for {col}",
+                title_x=0.5,
+                xaxis_title="Time",
+                yaxis_title="Value",
+            )
+            fig.write_html(f"{self.path}/{col}.html")
+
+
+"""
+Example usage
+
+service = Seq2SeqService()
+service.train(epochs=100, batch_size=32)
+service.generate_plots()
+"""

600abfb40397f075642e758551705d038e8bad2e
Octavian Gavril <100153009+octaviangavril@users.noreply.github.com>
2024-01-20 16:57:41 +0200

    fix(ai-models): refactor seq2seq model with Tranformer layers

--- src/seq2seq/seq2seqService.py
+++ src/seq2seq/seq2seqService.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, LSTM, Dense, RepeatVector, TimeDistributed
+from tensorflow.keras.layers import Input, Dense, Dropout, LayerNormalization, TimeDistributed
+from tensorflow.keras.layers import MultiHeadAttention  # Import direct din TensorFlow
 import matplotlib.pyplot as plt
 from tensorflow.keras.optimizers import Adam
 
@@ -21,7 +22,6 @@ df.set_index('Timestamp', inplace=True)
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(df)
 
-
 def create_sequences(data, n_steps_in, n_steps_out):
     X, y = [], []
     for i in range(len(data)):
@@ -34,7 +34,6 @@ def create_sequences(data, n_steps_in, n_steps_out):
         y.append(seq_y)
     return np.array(X), np.array(y)
 
-
 n_steps_in = 168
 n_steps_out = 168
 
@@ -43,21 +42,28 @@ test_data = scaled_data[-n_steps_out:]
 
 X_train, y_train = create_sequences(train_data, n_steps_in, n_steps_out)
 
+# Define the input layer
 input_seq = Input(shape=(n_steps_in, len(columns_to_predict)))
-encoder_lstm = LSTM(64, activation='tanh', return_state=True)
-_, state_h, state_c = encoder_lstm(input_seq)
 
-decoder_input = RepeatVector(n_steps_out)(state_h)
-decoder_lstm = LSTM(64, activation='tanh', return_sequences=True)(decoder_input)
-output = TimeDistributed(Dense(len(columns_to_predict)))(decoder_lstm)
+# Transformer Encoder
+encoder_output = MultiHeadAttention(num_heads=2, key_dim=len(columns_to_predict)//2)(query=input_seq, value=input_seq)
+encoder_output = Dropout(0.1)(encoder_output)
+encoder_output = LayerNormalization(epsilon=1e-6)(input_seq + encoder_output)
+
+# Transformer Decoder
+decoder_output = MultiHeadAttention(num_heads=2, key_dim=len(columns_to_predict)//2)(query=encoder_output, value=encoder_output)
+decoder_output = Dropout(0.1)(decoder_output)
+decoder_output = LayerNormalization(epsilon=1e-6)(encoder_output + decoder_output)
+
+# Output layer
+output = TimeDistributed(Dense(len(columns_to_predict)))(decoder_output)
 
 model = Model(inputs=input_seq, outputs=output)
 model.compile(optimizer=Adam(learning_rate=0.01), loss='mse')
 
 model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2)
 
-new_data = np.array([test_data[:n_steps_in]])
-new_predictions = model.predict(new_data)
+new_predictions = model.predict(train_data[-n_steps_in:].reshape(1, n_steps_in, len(columns_to_predict)))
 
 predicted_values_original = scaler.inverse_transform(new_predictions.reshape(-1, len(columns_to_predict)))
 test_data_original = scaler.inverse_transform(test_data)
@@ -66,8 +72,8 @@ for i, col in enumerate(columns_to_predict):
     plt.figure(figsize=(12, 4))
 
     prediction_times = pd.date_range(start=df.index[-n_steps_in], periods=n_steps_out+1, freq='H')[1:]
-    plt.plot(prediction_times, test_data_original[-n_steps_out:, i], label=f'Actual {col}', linestyle='-', marker='o')
-    plt.plot(prediction_times, predicted_values_original[:, i], label=f'Predicted {col}', linestyle='--', marker='o')
+    plt.plot(prediction_times, test_data_original[:, i], label=f'Actual {col}', linestyle='-', marker='')
+    plt.plot(prediction_times, predicted_values_original[:, i], label=f'Predicted {col}', linestyle='-', marker='')
 
     plt.title(f'Evoluția valorii {col} în ultima săptămână')
     plt.xlabel('Timestamp')

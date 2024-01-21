ec81cefec41b01c07407104eae88bcd00ca75260
Octavian Gavril <100153009+octaviangavril@users.noreply.github.com>
2024-01-21 14:12:24 +0200

    fix(seq2seq): add dataframe argument, modify plots, remove data split

--- src/seq2seq/seq2seq_service.py
+++ src/seq2seq/seq2seq_service.py
@@ -9,12 +9,22 @@ from tensorflow.keras.layers import (
     LayerNormalization,
     TimeDistributed,
 )
-from tensorflow.keras.layers import MultiHeadAttention  # Import direct din TensorFlow
-import matplotlib.pyplot as plt
+from tensorflow.keras.layers import MultiHeadAttention  
 from tensorflow.keras.optimizers import Adam
 import plotly.graph_objects as go
 
-from src.extractor import extract_dataset
+
+def _create_sequences(data, n_steps_in, n_steps_out):
+    X, y = [], []
+    for i in range(len(data)):
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out
+        if out_end_ix > len(data):
+            break
+        seq_x, seq_y = data[i:end_ix], data[end_ix:out_end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
 
 
 class Seq2SeqService:
@@ -22,32 +32,16 @@ class Seq2SeqService:
     Service for constructing, training and generating plots for the Seq2Seq model
     """
 
-    def __init__(self):
-        self.data = extract_dataset()
+    def __init__(self, data: pd.DataFrame):
+        self.predicted_values_original = None
+        self.test_data = None
         self.path = "../../static/predictions_plots/seq2seq"
 
         self.n_steps_in = 168
         self.n_steps_out = 168
 
-        self.columns_to_predict = [
-            "pres",
-            "temp1",
-            "umid",
-            "temp2",
-            "V450",
-            "B500",
-            "G550",
-            "Y570",
-            "O600",
-            "R650",
-            "temps1",
-            "temps2",
-            "lumina",
-        ]
-        self.df = pd.DataFrame(
-            data[1:], columns=["Timestamp"] + self.columns_to_predict
-        )
-
+        self.columns_to_predict = data.columns[1:]
+        self.df = data
         self.df["Timestamp"] = pd.to_datetime(self.df["Timestamp"])
         self.df[self.columns_to_predict] = self.df[self.columns_to_predict].apply(
             pd.to_numeric
@@ -57,10 +51,9 @@ class Seq2SeqService:
         self.scaler = MinMaxScaler(feature_range=(0, 1))
         self.scaled_data = self.scaler.fit_transform(self.df)
 
-        self.train_data = self.scaled_data[: -self.n_steps_out]
-        self.test_data = self.scaled_data[-self.n_steps_out :]
+        self.train_data = self.scaled_data
 
-        self.X_train, self.y_train = self._create_sequences(
+        self.X_train, self.y_train = _create_sequences(
             self.train_data, self.n_steps_in, self.n_steps_out
         )
 
@@ -90,17 +83,7 @@ class Seq2SeqService:
             decoder_output
         )
 
-    def _create_sequences(self, data, n_steps_in, n_steps_out):
-        X, y = [], []
-        for i in range(len(data)):
-            end_ix = i + n_steps_in
-            out_end_ix = end_ix + n_steps_out
-            if out_end_ix > len(data):
-                break
-            seq_x, seq_y = data[i:end_ix], data[end_ix:out_end_ix]
-            X.append(seq_x)
-            y.append(seq_y)
-        return np.array(X), np.array(y)
+        self.model = Model(inputs=self.input_seq, outputs=self.output)
 
     def train(self, epochs: int = 100, batch_size: int = 32):
         """
@@ -113,7 +96,6 @@ class Seq2SeqService:
         Return: None
         """
 
-        self.model = Model(inputs=self.input_seq, outputs=self.output)
         self.model.compile(optimizer=Adam(learning_rate=0.01), loss="mse")
 
         self.model.fit(
@@ -124,8 +106,18 @@ class Seq2SeqService:
             validation_split=0.2,
         )
 
+    def predict(self, test_data: pd.DataFrame):
+        """
+        Predicts the values for the next 168 hours
+
+        Return: None
+        """
+        self.test_data = test_data
+        self.test_data["Timestamp"] = pd.to_datetime(self.test_data["Timestamp"])
+        self.test_data.set_index("Timestamp", inplace=True)
+
         new_predictions = self.model.predict(
-            self.train_data[-self.n_steps_in :].reshape(
+            self.train_data[-self.n_steps_in:].reshape(
                 1, self.n_steps_in, len(self.columns_to_predict)
             )
         )
@@ -133,7 +125,6 @@ class Seq2SeqService:
         self.predicted_values_original = self.scaler.inverse_transform(
             new_predictions.reshape(-1, len(self.columns_to_predict))
         )
-        self.test_data_original = self.scaler.inverse_transform(self.test_data)
 
     def generate_plots(self):
         """
@@ -145,15 +136,15 @@ class Seq2SeqService:
             fig = go.Figure()
             fig.add_trace(
                 go.Scatter(
-                    x=self.df.index[-self.n_steps_in :],
-                    y=self.df[col].values[-self.n_steps_in :],
+                    x=self.test_data.index,
+                    y=self.test_data.loc[:, col],
                     mode="lines",
                     name=f"Actual {col}",
                 )
             )
             fig.add_trace(
                 go.Scatter(
-                    x=self.df.index[-self.n_steps_in :],
+                    x=self.test_data.index,
                     y=self.predicted_values_original[:, i],
                     mode="lines",
                     name=f"Predicted {col}",
@@ -168,10 +159,11 @@ class Seq2SeqService:
             fig.write_html(f"{self.path}/{col}.html")
 
 
-"""
-Example usage
+# Example usage
 
-service = Seq2SeqService()
+data = pd.read_csv("../../static/SensorMLDataset_small.csv")
+service = Seq2SeqService(data)
 service.train(epochs=100, batch_size=32)
+test_data = pd.read_csv("../../static/SensorMLTestDataset.csv")
+service.predict(test_data)
 service.generate_plots()
-"""

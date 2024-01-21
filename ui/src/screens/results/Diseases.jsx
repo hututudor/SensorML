a53ef0b556e4978eef9e25d787d5fab6ffa90f33
Hutu Tudor <hutu.tudor@yahoo.com>
2024-01-21 22:42:56 +0200

    feat(ui): display probabilities in percentages

--- ui/src/screens/results/Diseases.jsx
+++ ui/src/screens/results/Diseases.jsx
@@ -8,6 +8,14 @@ import {
   Td,
 } from '@chakra-ui/react';
 
+const formatProbability = probability => {
+  if (!probability) {
+    return '-';
+  }
+
+  return `${(probability * 100).toFixed(2)}%`;
+};
+
 export const Diseases = ({ predictions }) => (
   <TableContainer>
     <Table>
@@ -20,23 +28,23 @@ export const Diseases = ({ predictions }) => (
       <Tbody>
         <Tr>
           <Td>Early Blight</Td>
-          <Td>{predictions?.early_blight ?? '-'}</Td>
+          <Td>{formatProbability(predictions?.early_blight)}</Td>
         </Tr>
         <Tr>
           <Td>Gray Mold</Td>
-          <Td>{predictions?.gray_mold ?? '-'}</Td>
+          <Td>{formatProbability(predictions?.gray_mold)}</Td>
         </Tr>
         <Tr>
           <Td>Late Blight</Td>
-          <Td>{predictions?.late_blight ?? '-'}</Td>
+          <Td>{formatProbability(predictions?.late_blight)}</Td>
         </Tr>
         <Tr>
           <Td>Leaf Mold</Td>
-          <Td>{predictions?.leaf_mold ?? '-'}</Td>
+          <Td>{formatProbability(predictions?.leaf_mold)}</Td>
         </Tr>
         <Tr>
           <Td>Powdery Mildew</Td>
-          <Td>{predictions?.powdery_mildew ?? '-'}</Td>
+          <Td>{formatProbability(predictions?.powdery_mildew)}</Td>
         </Tr>
       </Tbody>
     </Table>
